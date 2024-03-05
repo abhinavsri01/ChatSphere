f6595c7bf8a7d12b961e7adcd39a15dee0a994fa
Abhinav Srivastava <srivastavaabhinav2019@gmail.com>
2024-03-05 07:11:37 +0530

    first commit

---
+++ backend/frontend/src/components/userAvatar/UserListItem.js
@@ -0,0 +1,44 @@
+import { Avatar } from "@chakra-ui/avatar";
+import { Box, Text } from "@chakra-ui/layout";
+import { ChatState } from "../../Context/ChatProvider";
+
+const UserListItem = ({ user, handleFunction }) => {       // added user before handleFunction  by ABHINAV
+  // const { user } = ChatState();        this line commented in video 
+
+  return (
+    <Box
+      onClick={handleFunction}
+      cursor="pointer"
+      bg="#E8E8E8"
+      _hover={{
+        background: "#38B2AC",
+        color: "white",
+      }}
+      w="100%"
+      d="flex"
+      alignItems="center"
+      color="black"
+      px={3}
+      py={2}
+      mb={2}
+      borderRadius="lg"
+    >
+      <Avatar
+        mr={2}
+        size="sm"
+        cursor="pointer"
+        name={user.name}
+        src={user.pic}
+      />
+      <Box>
+        <Text>{user.name}</Text>
+        <Text fontSize="xs">
+          <b>Email : </b>
+          {user.email}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default UserListItem;
